757779efd515dda592305fe79e1e287ca6e81ea3
Paul M. Christian <paulmchristian@gmail.com>
2021-08-04 20:10:07 -0500

    refactor scad

--- src/musicality/scad.clj
+++ src/musicality/scad.clj
@@ -3,7 +3,24 @@
   (:use [scad-clj.model])
   (:use [scad-clj.text]))
 
+(defn output [scad-data]
+  (->> scad-data
+       (write-scad)
+       (spit "3d-print/output.scad")))
+
+
+#_(->> (cube 1 2 3 :center true)
+     (output))
+
+
+#_(->> (sphere 5)
+     (with-fn 10)
+     (output))
+
+
+
 (def pitch-class-order [:0 :1 :2 :3 :4 :5 :6 :7 :8 :9 :૪ :Ɛ])
+
 
 (defn pitch-class "extrudes the character for a pitch-class"
   [pc & {:keys [h size]}]
@@ -16,6 +33,8 @@
                         :halign "center"
                         :valign "center")))
 
+
+
 (defn translate-on-clockface "translates an object to a position at n-o'clock at radius r"
   [r n obj]
   (let [theta (* n (/ Math/PI 6))]
@@ -24,7 +43,12 @@
                 0]
                obj)))
 
-(defn arc-vertices "returns a vec of [x,y] consisting of vertices of an arc"
+#_(->> (range 12)
+     (map #(->> (pitch-class (nth pitch-class-order %) :h 0.1 :size 1)
+                (translate-on-clockface 5 %)))
+     (output))
+
+(defn arc-vertices "returns a vec of [[x y] ...] consisting of vertices of an arc"
   [r1 r2 arc-length center-theta segs]
   (let [seg-length (/ arc-length segs)
         verts (range (+ 1 segs))
@@ -32,7 +56,7 @@
                           (let [theta (+ center-theta
                                          (- (* seg seg-length)
                                             (/ arc-length 2)))]
-                            (map #(* r %) [(Math/sin theta) (Math/cos theta)])))]
+                            [(* r (Math/sin theta)) (* r (Math/cos theta))]))]
     (concat (->> verts
                  (map #(seg-to-arc-vert % r2)))
             (->> verts
@@ -41,86 +65,103 @@
             [(seg-to-arc-vert 0 r2)]
             )))
 
-(defn ring-of-spheres [r r-sphere & {:keys [no-rotate?]}]
+#_(->> (arc-vertices 1 3 (/ Math/PI 3) 0 3)
+       (polygon)
+       (output))
+
+(defn ring-of-spheres  "returns 12 spheres of radius r-sphere arranged in a ring of radius r. no-rotate? specifies whether to rotate each sphere such that its pole is aligned with the ring's center"
+  [r r-sphere & {:keys [no-rotate?]}]
   (->> (range 12)
-       (map #(translate-on-clockface r %
-                                     (rotate [(if no-rotate?
-                                                0
-                                                (/ Math/PI 2))
-                                              0
-                                              (* % (/ Math/PI -6))]
-                                             (sphere r-sphere))))
+       (map #(translate-on-clockface
+              r %
+              (->> (sphere r-sphere)
+                   (rotate [(if no-rotate?
+                              0
+                              (/ Math/PI 2))
+                            0
+                            (* % (/ Math/PI -6))]))))
        (union)))
 
+#_(output (ring-of-spheres 5 1 :no-rotate? false))
 
-(defn hub [inner outer & {:keys [clockface-thickness sphere-radius hub-radius spacer-ring-thickness]}]
-  (let [gap-radius 0.25
-        height (* sphere-radius 2.5)
-        sphere-gap-radius 0.2
+
+
+
+(defn hub-height [sphere-radius] (* sphere-radius 2.5))
+(def hub-gap-radius 0.25)
+(def sphere-gap-radius 0.2)
+
+(defn hub-inner [& {:keys [clockface-thickness
+                           sphere-radius
+                           hub-radius
+                           spacer-ring-thickness]}]
+  
+  (let [r-ring (- hub-radius (* 1.5 sphere-radius))
+        r-cup (+ sphere-radius sphere-gap-radius)]
+    
+    (-> (with-fn 144 
+                  (cylinder hub-radius
+                            (+ (hub-height sphere-radius)
+                               clockface-thickness
+                               spacer-ring-thickness)
+                            :center false))
+        
+        (difference 
+         (with-fn 36
+           (union
+            ;; cup
+            (->> (ring-of-spheres r-ring r-cup :no-rotate? true)
+                 (translate [0 0 r-cup]))
+            
+            ;; aperture
+            (ring-of-spheres r-ring (* 1.075 sphere-radius) :no-rotate? true)))))))
+
+#_(->>  (hub-inner :clockface-thickness 1
+                   :sphere-radius 0.2
+                   :hub-radius 2
+                   :spacer-ring-thickness 0.5)
+        (output))
+
+
+(defn hub-outer [& {:keys [clockface-thickness sphere-radius hub-radius spacer-ring-thickness]}]
+  (let [height (hub-height sphere-radius)
         outer-z (* sphere-radius 1.25)
-        ]
 
-    (with-fn 144
-      (union
+        body (with-fn 36
+               (union (with-fn 144 (cylinder 12 0.4 :center false))
+                      (->> (ring-of-spheres
+                            (+ (* sphere-radius 0.5) hub-radius)
+                            (* 2.2 sphere-radius))
+                           (scale [1 1 0.75])
+                           (translate [0 0 outer-z]))))
 
-       ;; Inner Hub
-       (when inner
-
-         (let [r (- hub-radius (* 1.5 sphere-radius))
-               cup-r (+ sphere-radius sphere-gap-radius)]
-
-           (as-> (cylinder hub-radius (+ height clockface-thickness spacer-ring-thickness) :center false) v
-             (with-fn 36
-             (difference v 
-                         (union
-                          ;; cup
-                          (translate [0 0 cup-r]
-                                     (ring-of-spheres r
-                                                      cup-r
-                                                      :no-rotate? true))
-                          
-                          ;; aperture
-                          (ring-of-spheres r
-                                           (* 1.075 sphere-radius)
-                                           :no-rotate? true))))
-             )))
-
-       #_(ring-of-spheres 7.4 sphere-radius)
-
-
-       ;; Outer Hub
-       (when outer
-         
-
-                    ;; buckyballs (don't print)
-
-         #_(translate [0 0 outer-z] 
-                    (color [1 0 1 1]
-                           (ring-of-spheres (+ hub-radius sphere-radius) sphere-radius)))
-                    
-
-         (difference
-          (with-fn 36
-            (difference
-             (union (with-fn 144 (cylinder 12 0.4 :center false))
-                    (translate [0 0 outer-z]
-                               
-                               (scale [1 1 0.75] (ring-of-spheres (+ (* sphere-radius 0.5) hub-radius)
-                                                                  (* 2.2 sphere-radius)))))
-
-             (translate [0 0 outer-z]
-                        (union
+        cavities (with-fn 36
+                   (->> (union
                          ;; cup
                          (ring-of-spheres (+ (* 1.1 sphere-radius) hub-radius)
                                           (+ sphere-radius
                                              sphere-gap-radius))
                          ;; aperture
-                         (ring-of-spheres hub-radius (* 1.075 sphere-radius))))))
-                                        ;(cylinder 13.5 4)
-          (cylinder (+ hub-radius gap-radius) (* height 10)) ; inner circle
-          (translate [0 0 height]  ; top-crop
-                     (cylinder 12 1 :center false))
-))))))
+                         (ring-of-spheres hub-radius (* 1.075 sphere-radius)))
+                        (translate [0 0 outer-z])))]
+
+    (-> body
+        (difference cavities)
+
+        ; cutout for hub-inner
+       (difference (with-fn 144
+                      (cylinder (+ hub-radius hub-gap-radius) (* height 10))))
+        ; crop the top
+        (difference (->> (cylinder 12 1 :center false)
+                         (translate [0 0 height]))))))
+
+
+#_(->>  (hub-outer :clockface-thickness 0.1
+                   :sphere-radius 0.5
+                   :hub-radius 2
+                   :spacer-ring-thickness 0.5)
+        (output))
+
 
 (defn chroma-circle
   [clockface mask
@@ -158,21 +199,6 @@
                                                   )))))
 
 
-                   ;; Scale degree chord
-                   #_(->> [[0 "I"] [2 "ii"] [4 "iii"] [5 "IV"] [7 "V"] [9 "vi"] [11 "vii°"]]
-                        (map (fn [[pc scale-degree]]
-                               (->> (text scale-degree
-                                          :font "JetBrains Mono"
-                                          :size scale-degree-font-size :halign "center" :valign "center")
-                                    (extrude-linear {:height 0.2 :center false})
-                                    (color [0 0 0 1])
-                                    (rotate [0 0 (* pc (/ Math/PI -6))])
-                                    (translate-on-clockface scale-degree-radius #_(- pitch-class-radius
-                                                                                     pitch-class-font-size
-                                                                                     scale-degree-font-size)
-                                                            pc)
-                                    (translate [0 0 0.4])))))
-
                    ;; deltas -
                    (->> pitch-classes
                         (map (fn [pc]
@@ -203,49 +229,6 @@
                                       (translate [0 0 0.4]))))))
 
                    
-                   
-
-                   ;; Scale mask
-                   #_(with-fn 144
-                     (as-> (cylinder (+ 1 pitch-class-radius border) 0.4 :center false) v
-                       
-                       (color [1 1 1 0.5] v)
-                       (difference v (cylinder 12 1))          
-                       (difference v
-                                   (->> [0 2 4 5 7 9 11] ; PC window
-                                        (map (fn [pc]
-                                               (->> (arc-vertices
-                                                     (- pitch-class-radius (- pitch-class-font-size 1))
-                                                     (+ pitch-class-radius (- pitch-class-font-size 1))
-                                                     (* 0.9 (/ Math/PI 6))
-                                                     (* pc
-                                                        (/ (* 2 Math/PI) 12))
-                                                     12)
-                                                    (polygon)
-                                                    (extrude-linear {:height 2}))))))
-                       (union v (->> [0 2 4 5 7 9 11] ; PC border
-                                     (map (fn [pc] 
-                                            (->> (arc-vertices
-                                                  (- pitch-class-radius (- pitch-class-font-size 1) 0.3)
-                                                  (+ pitch-class-radius (- pitch-class-font-size 1) 0.3)
-                                                  (* 0.95 (/ Math/PI 6))
-                                                  (* pc
-                                                     (/ (* 2 Math/PI) 12))
-                                                  12)
-                                                 (concat (reverse (arc-vertices
-                                                                   (- pitch-class-radius (- pitch-class-font-size 1))
-                                                                   (+ pitch-class-radius (- pitch-class-font-size 1))
-                                                                   (* 0.9 (/ Math/PI 6))
-                                                                   (* pc
-                                                                      (/ (* 2 Math/PI) 12))
-                                                                   12)))
-                                                 (polygon)
-                                                 (extrude-linear {:height 0.2 :center false})
-                                                 (translate [0 0 0.4])
-                                                 (color [0 0 0 1]))))))
-
-                       ))
-
                    ;; chromatic mask
                    (with-fn 144
                      (as-> (cylinder (+ 1.2 pitch-class-radius border scale-degree-font-size) 0.4 :center false) v
@@ -288,13 +271,13 @@
                        ))
 
                    ;; outer hub
-                   (color [1 1 1 0.75]
-                          (difference (hub false true
+                   (color [0 0 0 0.75]
+                          (difference (hub-outer
                                            :clockface-thickness clockface-thickness
                                            :sphere-radius sphere-radius
                                            :hub-radius hub-radius
                                            :spacer-ring-thickness spacer-ring-thickness)
-                                      (translate [0 0 -10] (cylinder pitch-class-radius 10 :center false))))
+                                      (translate [0 0 -10] (cylinder (/ pitch-class-radius 2) 10 :center false))))
 
                    
 
@@ -317,7 +300,7 @@
 
 
         ;; Inner hub
-        (hub true false
+        (hub-inner
              :clockface-thickness clockface-thickness
              :sphere-radius sphere-radius
              :hub-radius hub-radius
@@ -364,45 +347,66 @@
             )))))))
 
 
-(spit "3d-print/output.scad"
-      (write-scad (chroma-circle nil 1
-                                 :border 5
-                                 :clockface-thickness 0.3
-                                 :pitch-classes [0 4 7 10]
-                                 :pitch-class-font-size 4.5
-                                 :pitch-class-font-thickness 0.3
-                                 :pitch-class-radius 24
-                                 :scale-degree-font-size 3.4
-                                 :scale-degree-radius 17
-                                 :sphere-radius 1.25)))
+
+
+
 
 ;;; Big clock for 0.6 nozzle
-#_(spit "3d-print/output.scad"
-      (write-scad (chroma-circle 1 nil
-                                 :border 18
-                                 :clockface-thickness 0.6
-                                 :pitch-class-font-size 18
-                                 :pitch-class-font-thickness 2
-                                 :pitch-class-radius 70
-                                 :scale-degree-font-size 3.4
-                                 :scale-degree-radius 17
-                                 :sphere-radius 1.5)))
+#_(output (chroma-circle 1 nil
+                    :border 18
+                    :clockface-thickness 0.6
+                    :pitch-class-font-size 18
+                    :pitch-class-font-thickness 2
+                    :pitch-class-radius 70
+                    :scale-degree-font-size 3.4
+                    :scale-degree-radius 17
+                    :sphere-radius 1.5))
+
+
 
 
 ;; TODO -- move inner-hub cavities in slightly
 ;; TODO -- add some padding to inner-hub to move the bottom up even with outer ring
 
 
-#_(spit "output.scad"
-        (write-scad (union
-                     (intersection
-                      (->> (arc-vertices 2 10 (/ Math/PI 6) 0 12)
-                           (polygon)
-                           (extrude-linear {:height 5}))
-                      (hub true false))
+#_(->> (union
+      (intersection
+       (->> (arc-vertices 2 10 (/ Math/PI 6) 0 12)
+            (polygon)
+            (extrude-linear {:height 5}))
+       (hub true false))
+      
+      (intersection
+       (->> (arc-vertices 2 200 (/ Math/PI 6) 0 12)
+            (polygon)
+            (extrude-linear {:height 5}))
+       (hub true true)))
+     (output))
 
-                     (intersection
-                      (->> (arc-vertices 2 200 (/ Math/PI 6) 0 12)
-                           (polygon)
-                           (extrude-linear {:height 5}))
-                      (hub true true)))))
+
+
+
+; mask for 0.2mm nozzle
+(output (chroma-circle nil 1
+                         :border 5
+                         :clockface-thickness 0.3
+                         :pitch-classes [0 2 4 6 8 10 ]
+                         :pitch-class-font-size 4.5
+                         :pitch-class-font-thickness 0.3
+                         :pitch-class-radius 24
+                         :scale-degree-font-size 3.4
+                         :scale-degree-radius 17
+                         :sphere-radius 1.25))
+
+; clockface for 0.2mm nozzle
+#_(->> (chroma-circle 1 nil
+                      :border 5
+                      :clockface-thickness 0.3
+                      :pitch-classes [0 4 7 10]
+                      :pitch-class-font-size 4.5
+                      :pitch-class-font-thickness 0.3
+                      :pitch-class-radius 24
+                      :scale-degree-font-size 3.4
+                      :scale-degree-radius 17
+                      :sphere-radius 1.25)
+       (output))
