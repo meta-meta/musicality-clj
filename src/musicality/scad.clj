d68934aa63ce6d56fbcad5a78de16c5dac592c7b
Paul M. Christian <paulmchristian@gmail.com>
2021-08-09 23:09:33 -0500

    add mu major mask

--- src/musicality/scad.clj
+++ src/musicality/scad.clj
@@ -1,7 +1,12 @@
 (ns musicality.scad
   (:use [scad-clj.scad])
   (:use [scad-clj.model])
-  (:use [scad-clj.text]))
+  (:use [scad-clj.text])
+  (:use [musicality.pc-sets])
+  (:use [clojure.repl]))
+
+
+
 
 (defn output [scad-data]
   (->> scad-data
@@ -27,16 +32,17 @@
                         :valign "center")))
 
 (defn translate-on-clockface "translates an object to a position at n-o'clock at radius r"
-  [r n obj]
+  [r n rotate? obj]
   (let [theta (* n (/ Math/PI 6))]
-    (translate [(* r (Math/sin theta))
-                (* r (Math/cos theta))
-                0]
-               obj)))
+    (->> obj
+         (rotate [0 0 (if rotate? (* -1 theta) 0)])
+         (translate [(* r (Math/sin theta))
+                     (* r (Math/cos theta))
+                     0]))))
 
 #_(->> (range 12)
        (map #(->> (pitch-class (nth pitch-class-order %) :h 0.1 :size 1)
-                  (translate-on-clockface 5 %)))
+                  (translate-on-clockface 5 false %)))
        (output))
 
 (defn arc-vertices "returns a vec of [[x y] ...] consisting of vertices of an arc"
@@ -63,13 +69,14 @@
   [r r-sphere & {:keys [no-rotate?]}]
   (->> (range 12)
        (map #(translate-on-clockface
-              r %
+              r % true
               (->> (sphere r-sphere)
                    (rotate [(if no-rotate?
                               0
                               (/ Math/PI 2))
                             0
-                            (* % (/ Math/PI -6))]))))
+                            (* % (/ Math/PI -6))]))
+               ))
        (union)))
 
 #_(output (ring-of-spheres 5 1 :no-rotate? false))
@@ -126,11 +133,12 @@
         outer-z (* sphere-radius 1.25)
 
         body (with-fn 36
-               (union (with-fn 144 (cylinder 12 0.4 :center false))
+               (union (with-fn 144 (color [1 1 1 0.5] (cylinder 12 0.4 :center false)))
                       (->> (ring-of-spheres
                             (+ (* sphere-radius 0.5) hub-radius)
                             (* 2.2 sphere-radius))
                            (scale [1 1 0.75])
+                           (color [0 0 0 0.75])
                            (translate [0 0 outer-z]))))
 
         cavities (with-fn 36
@@ -173,7 +181,7 @@
                             :h font-thickness
                             :size font-size)
                (color [0 0 0 1])
-               (translate-on-clockface radius i))))))
+               (translate-on-clockface radius i false))))))
 
 (defn pc-clockface
   [& {:keys [border
@@ -239,6 +247,7 @@
   [& {:keys [border
              clockface-thickness
              pitch-classes
+             zero-label?
              pitch-class-font-thickness
              pitch-class-radius
              pitch-class-font-size
@@ -267,28 +276,34 @@
 
                  ;; deltas -
                  (->> pitch-classes
+                      (filter #(if zero-label? true (not= 0 %)))
                       (map (fn [pc]
                              (->> (text (str "-" (mod (- 12 pc) 12))
                                         :font "JetBrains Mono"
                                         :size (* 0.8 scale-degree-font-size) :halign "center" :valign "center")
                                   (extrude-linear {:height 0.2 :center false})
                                   (color [0 0 0 1])
-                                  (rotate [0 0 (* pc (/ Math/PI -6))])
-                                  (translate-on-clockface scale-degree-radius
-                                                          pc)
+                                  (translate-on-clockface
+                                   scale-degree-radius
+                                   pc
+                                   true)
                                   (translate [0 0 0.4])))))
 
                  ;; deltas +
                  (->> pitch-classes
-                      (map (fn [pc]
+                     (filter #(if zero-label? true (not= 0 %)))
+                     (map (fn [pc]
                              (->> (text (str "+" pc)
                                         :font "JetBrains Mono"
                                         :size (* 0.8 scale-degree-font-size) :halign "center" :valign "center")
                                   (extrude-linear {:height 0.2 :center false})
                                   (color [0 0 0 1])
-                                  (rotate [0 0 (* pc (/ Math/PI -6))])
-                                  (translate-on-clockface (+ (* 0.7 scale-degree-font-size) pitch-class-radius pitch-class-font-size)
-                                                          pc)
+                                  (translate-on-clockface 
+                                   (+ (* 0.7 scale-degree-font-size)
+                                      pitch-class-radius
+                                      pitch-class-font-size)
+                                   pc
+                                   true)
                                   (translate [0 0 0.4])))))
 
 
@@ -340,7 +355,7 @@
                                    :spacer-ring-thickness spacer-ring-thickness)
                                   (->> (cylinder (/ pitch-class-radius 2) 10 :center false)
                                        (translate [0 0 -10] )))
-                      (color [0 0 0 0.75]))))
+                      )))
 
     )))
 
@@ -414,7 +429,70 @@
         :sphere-radius 1.25)
        (output))
 
-(->>
+
+(defn str-on-clockface [{:keys [h r s font font-size clock-time] :as opts}]
+  (->> (text s
+             :font font
+             :size font-size :halign "center" :valign "center")
+       (extrude-linear {:height h :center false})
+       (translate-on-clockface r clock-time true)
+       ))
+
+(defn mu-major []
+  (->> 
+   (apply difference
+          (pc-mask
+           :border 5
+           :clockface-thickness 0.3
+           :pitch-classes [0 2 4 7]
+           :pitch-class-font-size 4.5
+           :pitch-class-font-thickness 0.3
+           :pitch-class-radius 24
+           :scale-degree-font-size 3.4
+           :scale-degree-radius 17
+           :sphere-radius 1.25
+           :zero-label? false)
+          
+
+          (->> (clojure.string/split "mu-maj" #"")
+               (map-indexed (fn [i s]
+                              (->> (str-on-clockface
+                                    {:s s
+                                     :font (if (= s "μ") "JetBrains Mono" "Paper")
+                                     :font-size 3
+                                     :r 30
+                                     :clock-time (+ 11.4 (/ i 4))
+                                     :h 10
+                                     })
+                                   (color [0 0 0 1])))))
+
+          #_[(->> (str-on-clockface
+                   {:s "asd"
+                    :font-size 5
+                    :r 16
+                    :clock-time 10
+                    :h 10
+                    })
+                  (translate [0 0 0.4]))]
+          #_[(->> (text "μ-major"
+                        :font "JetBrains Mono"
+                        :size 5 :halign "center" :valign "center")
+                  (extrude-linear {:height 10 :center false})
+                  (color [0 0 0 1])
+                  #_(rotate [0 0 (* 9 (/ Math/PI -6))])
+                  (translate-on-clockface 16 9.5 true)
+                  (translate [0 0 0.4]))] )
+ )) 
+
+
+
+
+
+
+(->> (mu-major)
+     (output))
+
+#_(->>
  (pc-clockface
         :border 5
         :clockface-thickness 0.3
@@ -426,14 +504,5 @@
         :scale-degree-radius 17
         :sphere-radius 1.25)
  (translate [0 0 -1])
- (union (pc-mask
-         :border 5
-         :clockface-thickness 0.3
-         :pitch-classes [0 2 4 6 8 10]
-         :pitch-class-font-size 4.5
-         :pitch-class-font-thickness 0.3
-         :pitch-class-radius 24
-         :scale-degree-font-size 3.4
-         :scale-degree-radius 17
-         :sphere-radius 1.25))
+ (union (mu-major))
  (output))
