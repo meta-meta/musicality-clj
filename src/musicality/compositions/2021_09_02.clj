0f69de8be1f88dd9b5304e184eb149c5d127a847
Paul M. Christian <paulmchristian@gmail.com>
2021-09-03 18:47:46 -0500

    send pc-sets and metronome

--- src/musicality/compositions/2021_09_02.clj
+++ src/musicality/compositions/2021_09_02.clj
@@ -5,15 +5,16 @@
 
 #_(s/init)
 #_(s/deinit)
-(o/send "/pianoteq/oscMsg/host" "192.168.1.33")
-(o/send "/pianoteq/oscMsg/port" "9000")
-
-(s/send-beat "pianoteq" 1 :oscMsg ["/oled/print" "help" 0 0 35])
-(s/send-beat "pianoteq" 100 :oscMsg ["/oled/clear" 1])
+#_(o/send "/pianoteq/oscMsg/host" "192.168.1.33")
+#_(o/send "/pianoteq/oscMsg/port" "9000")
 
 
-(->> (range (/ 420 4))
-     (map #(* 4 %))
+#_(s/send-beat "pianoteq" 1 :oscMsg ["/oled/print" "help" 0 0 35])
+#_(s/send-beat "pianoteq" 100 :oscMsg ["/oled/clear" 1])
+
+
+#_(->> [0]
+     ;(map #(* 4 %))
      
      (map-indexed (fn [idx beat] 
                    
@@ -24,8 +25,45 @@
                     (s/send-beat "pianoteq" beat :oscMsg 
                                  (concat ["/led/set"]
                                          (->> (range 12)
-                                              (map (fn [i] [i (/ (mod (+ idx i) 12) 12) 1 0.01]))
+                                              (map (fn [i] [i (/ (mod (+ idx i) 12) 12) 1 0.005]))
                                               (flatten)))))))
+
+
+(defn send-beat [beat leds]
+
+ ;; TODO need multiple oscMsgs per beat
+ #_(s/send-beat "pianoteq" (+ 1 beat) :oscMsg
+               (concat ["/oled/print"]
+                       ()))
+
+  (s/send-beat "pianoteq" beat :oscMsg 
+               (concat ["/led/set"]
+                       (->> leds
+                            (map-indexed (fn [i v] [i (/ i 12) 1 (* v 0.005)]))
+                            (flatten)))))
+
+#_(send-beat 0 [1 0 0 0 1 0 0 1 0 0 1 0])
+#_(send-beat 0 [1 0 1 0 1 1 0 1 0 1 0 1])
+
+
+#_(->> (range (/ 420 35))
+     (map #(* 35 %))
+     (map-indexed (fn [idx beat] 
+                    (s/send-beat "pianoteq" beat :oscMsg 
+                                 (concat ["/led/add"]
+                                         (->> (range 12)
+                                              (map (fn [i] [i 0 0 0.004]))
+                                              (flatten))))
+
+                    (s/send-beat "pianoteq" (+ 1 beat) :oscMsg 
+                                 (concat ["/led/add"]
+                                         (->> (range 12)
+                                              (map (fn [i] [i 0 0 0]))
+                                              (flatten))))
+                    
+                    )))
+
+
 
 #_(s/send-beat "pianoteq" 0 :oscMsg ["/led/set" 0 0 0 1])
 #_(s/send-beat "pianoteq" 30 :oscMsg ["/led/set" 6 0.3 1 1])
